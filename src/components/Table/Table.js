c0586c255112de81d107a64ee9ab759f341a2ac4
Veselina Sidova <vessy.sidova@gmail.com>
2024-06-02 01:42:07 +0300

    modified fetch from API

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,44 +1,63 @@
 import React, { useEffect, useState } from 'react';
-import Loader from '../Loader/Loader'
+import { Container } from 'react-bootstrap';
+import styles from './Table.module.css';
+import Loader from '../Loader/Loader';
+import Person from '../Person/Person';
 
 const Table = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('https://swapi.dev/api/people')
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data.results);
+        const fetchAllPeople = async () => {
+            try {
+                let allPeople = [];
+                let nextPage = 'https://swapi.dev/api/people/';
+
+                while (nextPage) {
+                    const response = await fetch(nextPage);
+                    const result = await response.json();
+                    allPeople = allPeople.concat(result.results);
+                    nextPage = result.next;
+                }
+
+                setData(allPeople);
                 setLoading(false);
-            })
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        fetchAllPeople();
     }, []);
 
-    if (loading) return <Loader />;
-
     return (
-        <table className="data-table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Mass</th>
-                    <th>Height</th>
-                    <th>Hair Color</th>
-                    <th>Skin Color</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((person, index) => (  //no ID in person, data is not modified
-                    <tr key={index}>
-                        <td>{person.name}</td>
-                        <td>{person.mass}</td>
-                        <td>{person.height}</td>
-                        <td>{person.hair_color}</td>
-                        <td>{person.skin_color}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div>
+            {loading && <Loader />}
+            <div className={loading ? styles.blur : ''}>
+                <Container className="mt-5">
+                    <table className="data-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Mass</th>
+                                <th>Height</th>
+                                <th>Hair Color</th>
+                                <th>Skin Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((person, index) => (  //using index since there is no ID in SWAPI but we do not modify data
+                                <Person key={index} person={person} />
+                            ))}
+                        </tbody>
+
+                    </table>
+                </Container>
+            </div>
+        </div>
     );
 };
 
