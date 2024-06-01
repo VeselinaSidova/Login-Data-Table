d99ba4961a19ebfd5e69b080338c0b1aa98e5df9
Veselina Sidova <vessy.sidova@gmail.com>
2024-06-01 19:03:42 +0300

    fetch data from API

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,4 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
 const Table = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people')
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data.results);
+            })
+    }, []);
 
     return (
         <table className="data-table">
@@ -12,6 +23,15 @@ const Table = () => {
                 </tr>
             </thead>
             <tbody>
+                {data.map((person, index) => (  //no ID in person, data is not modified
+                    <tr key={index}>
+                        <td>{person.name}</td>
+                        <td>{person.mass}</td>
+                        <td>{person.height}</td>
+                        <td>{person.hair_color}</td>
+                        <td>{person.skin_color}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
