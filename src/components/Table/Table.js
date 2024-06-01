6caf0de80f77cff8ffec5ed130bf4cb64f915211
Veselina Sidova <vessy.sidova@gmail.com>
2024-06-01 19:02:51 +0300

    added table component

---
+++ src/components/Table/Table.js
@@ -0,0 +1,20 @@
+const Table = () => {
+
+    return (
+        <table className="data-table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Mass</th>
+                    <th>Height</th>
+                    <th>Hair Color</th>
+                    <th>Skin Color</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
