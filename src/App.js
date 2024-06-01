7975710ecfd326f93e290c0e46f1cb50d1314c82
Veselina Sidova <vessy.sidova@gmail.com>
2024-06-01 18:58:41 +0300

    changed file locations and spacing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Login from './components/Login';
-import Table from './components/Table';
+import Login from './components/Login/Login';
+import Table from './components/Table/Table';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/table" element={<Table />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <main id="main-content">
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/table" element={<Table />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
