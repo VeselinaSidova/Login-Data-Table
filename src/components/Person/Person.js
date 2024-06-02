9799b9ab59e9f22d059be95b3575ffc92babe580
Veselina Sidova <vessy.sidova@gmail.com>
2024-06-02 18:33:53 +0300

    modified person data

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Person = ({ person }) => {
+    const formatValue = (value, unit) => {
+        return value !== 'unknown' ? `${value} ${unit}` : value;
+    };
+
     return (
         <tr>
             <td>{person.name}</td>
-            <td>{person.mass}</td>
-            <td>{person.height}</td>
+            <td>{formatValue(person.mass, 'kg')}</td>
+            <td>{formatValue(person.height, 'cm')}</td>
             <td>{person.hair_color}</td>
             <td>{person.skin_color}</td>
         </tr>
